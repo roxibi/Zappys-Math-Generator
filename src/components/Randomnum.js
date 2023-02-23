6f6d4d2c7c0396eae093465dc0e3068405546c84
Roxana Bita <roxana.bita@gmail.com>
2023-02-23 23:07:04 +0100

    fix layout of page

--- src/components/Randomnum.js
+++ src/components/Randomnum.js
@@ -4,22 +4,23 @@ import "../App.css";
 
 const Randomnum = () => {
     const [arr, setArr] = useState([]);
+
     const createPDF = async () => {
         const pdf = new jsPDF("portrait", "pt", "a4");
         const data = await document.querySelector("#pdf");
         pdf.html(data).then(() => {
-          pdf.save(`Zappys.pdf`);
+            pdf.save(`Zappys.pdf`);
         });
-      };
+    };
 
     const nums = () => {
         let tempArray = [];
         const sign = "*";
         let num1;
         let num2;
-        while (tempArray.length < 50) {
-            num1 = Math.ceil(Math.random() * 15);
-            num2 = Math.ceil(Math.random() * 15);
+        while (tempArray.length < 60) {
+            num1 = Math.ceil(Math.random() * 12);
+            num2 = Math.ceil(Math.random() * 12);
 
             if (!tempArray.includes(num1 + sign + num2)) {
                 tempArray.push(num1 + sign + num2);
@@ -32,23 +33,25 @@ const Randomnum = () => {
     useEffect(() => {
         nums();
     }, []);
- 
+
     return (
-        <div id="pdf">
-             <button onClick={createPDF} type="button">Download</button>
-            {" "}
-            <h1>Zappys - generate Math Exercises</h1>
-            <p>Refresh the page to generate new exercises!</p>
-            <div className='grid-exercises'>
-                {arr.map((x) => (
-                    <div className="grid-item">{x}=</div>
-                ))}
+        <div>
+            <header>
+                <h1>Zappys - generate Math Exercises</h1>
+                <p>Refresh the page to generate new exercises!</p>
+            </header>
+            <div id='pdf'>
+                <div className='grid-exercises'>
+                    {arr.map((x) => (
+                        <div className='grid-item'>{x}=</div>
+                    ))}
+                </div>
             </div>
-           
+            <button onClick={createPDF} type='button'>
+                Download
+            </button>
         </div>
     );
 };
-
-
 
 export default Randomnum;
