c1c096c4597edcca1cc254f44c73a3ae073f7e40
Roxana Bita <roxana.bita@gmail.com>
2023-02-24 09:20:03 +0100

    style select

--- src/components/Randomnum.js
+++ src/components/Randomnum.js
@@ -1,16 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { jsPDF } from "jspdf";
 import "../App.css";
-import Logo from '../assets/Zappys-green.png'
+import Logo from "../assets/Zappys-green.png";
 
 const Randomnum = () => {
     const [arr, setArr] = useState([]);
-    const [sign, setSign] = useState('*');
-  
-  
-    const options =['*', '+', '-', '÷']
+    const [sign, setSign] = useState("*");
 
-
+    const options = ["*", "+", "-", "÷"];
 
     const createPDF = async () => {
         const pdf = new jsPDF("portrait", "pt", "a4");
@@ -22,7 +19,7 @@ const Randomnum = () => {
 
     const nums = () => {
         let tempArray = [];
-      
+
         let num1;
         let num2;
         while (tempArray.length < 60) {
@@ -39,19 +36,27 @@ const Randomnum = () => {
     console.log(arr);
     useEffect(() => {
         nums();
+        // eslint-disable-next-line
     }, [sign]);
 
     return (
         <div>
-            <div className="img">
-             <img className="logo" src={Logo} alt='Zappys Logo'/> </div>
+            <div className='img'>
+                <img className='logo' src={Logo} alt='Zappys Logo' />{" "}
+            </div>
             <header>
-               
                 <h1>Math worksheets generator</h1>
-                <p>Refresh the page to generate new exercises!</p>
-                <select onChange={(e)=>setSign(e.target.value)}>
-                   {options.map((o, index)=>{return <option key={index} value={o}>{o} </option>})}
-                </select>
+                <div className="sign-line">
+                <p>Select the operation</p>
+                <select onChange={(e) => setSign(e.target.value)}>
+                    {options.map((o, index) => {
+                        return (
+                            <option key={index} value={o}>
+                                {o}{" "}
+                            </option>
+                        );
+                    })}
+                </select></div>
             </header>
             <div id='pdf'>
                 <div className='grid-exercises'>
@@ -59,7 +64,9 @@ const Randomnum = () => {
                         <div className='grid-item'>{x}=</div>
                     ))}
                 </div>
-                <p className="copyright">© Zappys by Diana & <a href='https://roxana-bita-webdev.netlify.app/en'>Roxana Bita</a>, 2023</p>
+                <p className='copyright'>
+                    © Zappys by Diana & <a href='https://roxana-bita-webdev.netlify.app/en'>Roxana Bita</a>, 2023
+                </p>
             </div>
             <button onClick={createPDF} type='button'>
                 Download
